bd451c5c47b13f4b5c3c8a218a52641c07ed0a8b
LukaPark <solchan0106@ronfic.com>
2024-12-28 17:07:01 +0900

    주사위 고르기 문제 init..

---
+++ 주사위_고르기/main.js
@@ -0,0 +1,23 @@
+console.log(solution([[1, 2, 3, 4, 5, 6], [3, 3, 3, 3, 4, 4], [1, 3, 3, 4, 4, 4], [1, 1, 4, 4, 5, 5]]));
+// [1, 4]
+console.log(solution([[1, 2, 3, 4, 5, 6], [2, 2, 4, 4, 6, 6]]))
+// [2]
+console.log(solution([[40, 41, 42, 43, 44, 45], [43, 43, 42, 42, 41, 41], [1, 1, 80, 80, 80, 80], [70, 70, 1, 1, 70, 70]]))
+// [1, 3]
+
+function solution(dice) {
+    let answer = [];
+
+    const results = [];
+
+    const n = dice.length / 2;
+
+    // 1. n 개의 주사위를 선택하는 경우의 수 만큼 반복문?;
+
+
+    return answer;
+}
+
+/**
+ * 1. 입력받은 dice 의 길이 n 의 절반 만큼의
+ */
