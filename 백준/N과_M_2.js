8c74deb967cd613daa9a56a1a4e6164c2e0b1ccd
LukaPark <solchan0106@ronfic.com>
2025-01-02 19:11:49 +0900

    complete - 백준 > N과 M(2) - 조합 추출 함수 설명 추가

--- 백준/N과_M_2.js
+++ 백준/N과_M_2.js
@@ -7,14 +7,21 @@ main(4, 2);
 function main(n, m) {
     const targetArray = Array.from({ length: n }, (_,i) => i+1);
 
+    // 조합을 구하는 함수
     function getCombinations(arr, targetLength) {
         let result = [];
+        // 재귀의 마지막 부분, 목표 길이가 1인 경우 에는, 전달 받은 리스트를 순회하며 각 숫자를 하나의 배열로 반환.
         if (targetLength === 1) return arr.map((el) => [el]);
 
+        // arr에서 제일 앞 숫자는 고정하고, 나머지를 다시 조합.
         arr.forEach((fixed, idx, origin) => {
+            // 고정할 숫자를 제외한 배열의 나머지
             const rest = origin.slice(idx+1);
+            // 목표 길이를 - 1
             const combinations = getCombinations(rest, targetLength-1);
+            // 만들어진 조합을 고정 숫자와 다시 합치기.
             const attached = combinations.map((combination) => [fixed, ...combination]);
+            // 결과를 배열의 값만 모으기
             result.push(...attached);
         })
 
