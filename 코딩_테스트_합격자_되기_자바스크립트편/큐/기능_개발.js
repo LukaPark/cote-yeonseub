1c3a3dce0f56504dc614ab4c7fc66a86292c3219
LukaPark <solchan0106@ronfic.com>
2025-01-19 22:50:37 +0900

    solution - 큐 - 기능 개발

---
+++ 코딩_테스트_합격자_되기_자바스크립트편/큐/기능_개발.js
@@ -0,0 +1,93 @@
+/**
+ * progressess: 현재 진도율
+ * speeds: 하루에 진행하는 진도율
+ */
+class Node {
+    constructor(params) {
+        this.data = params;
+        this.next = null;
+    }
+
+    isComplete(day) {
+        let diff = 0;
+
+        if (this.data.progress + this.data.speed * day >= 100) {
+            diff++;
+            if (this.next) {
+                diff += this.next.isComplete(day);
+            }
+        }
+
+        return diff;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    push(item) {
+        const newNode = new Node(item);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.size++;
+    }
+
+    pop() {
+        if (!this.head) return null;
+
+        const removeNode = this.head;
+        this.head = this.head.next;
+
+        if (!this.head) this.tail = null;
+
+        this.size--;
+
+        return removeNode.data;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+}
+
+const solution = (progresses, speeds) => {
+    const res = [];
+    let cnt = 0;
+    let day = 0;
+    const queue = new Queue();
+
+    progresses.forEach((progress, idx) => {
+        queue.push({
+            progress: progress,
+            speed : speeds[idx],
+        })
+    });
+
+    while (cnt < progresses.length) {
+        let diff = queue.head.isComplete(day);
+        if (diff > 0) {
+            res.push(diff);
+        }
+        for (let i = 0; i < diff; i++) {
+            queue.pop()
+        }
+        cnt+=diff;
+        day++;
+    }
+
+    return res;
+}
+
+console.log(solution([2, 30, 1], [100, 100, 99]));
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
