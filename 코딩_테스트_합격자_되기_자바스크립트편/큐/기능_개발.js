795e6f012e07b97cdfdfb7a2a466b2a225f5ba80
LukaPark <solchan0106@ronfic.com>
2025-01-20 13:41:32 +0900

    solution - 큐 - 기능 개발 -> 다른 풀이 추가.

--- 코딩_테스트_합격자_되기_자바스크립트편/큐/기능_개발.js
+++ 코딩_테스트_합격자_되기_자바스크립트편/큐/기능_개발.js
@@ -89,5 +89,32 @@ const solution = (progresses, speeds) => {
     return res;
 }
 
-console.log(solution([2, 30, 1], [100, 100, 99]));
-console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
+console.log(solution2([93, 30, 55], [1, 30, 5]));
+// console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
+
+function solution2(progresses, speeds) {
+    const answer = [];
+    const n = progresses.length;
+
+    // 각 작업의 배포 가능일 계산.
+    const daysLeft = progresses.map((progress, idx) => Math.ceil((100 - progress) / speeds[idx]));
+
+    console.log(daysLeft);
+
+    let count = 0;
+    let maxDay = daysLeft[0];
+
+    for (let i = 0; i < n; i++) {
+        if (daysLeft[i] <= maxDay) {
+            count++;
+        } else {
+            answer.push(count);
+            count = 1;
+            maxDay = daysLeft[i];
+        }
+    }
+
+    answer.push(count);
+
+    return answer;
+}
